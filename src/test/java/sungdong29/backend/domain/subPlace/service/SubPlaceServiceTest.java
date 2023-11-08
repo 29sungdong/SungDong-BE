26efcef0ceb21c71f95fae7bcb975ed13d7dd989
doyeoo <ehdusla013@gmail.com>
2023-11-08 21:56:16 +0900

    [test] 시설 목록 조회 테스트

---
+++ src/test/java/sungdong29/backend/domain/subPlace/service/SubPlaceServiceTest.java
@@ -0,0 +1,26 @@
+package sungdong29.backend.domain.subPlace.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import sungdong29.backend.domain.subPlace.dto.response.SubPlacesResponseDTO;
+import sungdong29.backend.domain.subPlace.repository.SubPlaceRepository;
+import sungdong29.backend.domain.user.dto.request.UserRequestDTO;
+import sungdong29.backend.domain.user.dto.response.UserResponseDTO;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class SubPlaceServiceTest {
+    @Autowired
+    SubPlaceService subPlaceService;
+
+    @DisplayName("특정 장소 시설 조회 테스트")
+    @Test
+    void testFindSubPlacesSuccess(){
+        SubPlacesResponseDTO subPlacesResponseDTO = subPlaceService.findSubPlaces(1L);
+
+        assertThat(subPlacesResponseDTO.getSubPlaces()).isNotNull();
+    }
+}
