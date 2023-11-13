81ae3c9243707fddc0f70251bea5cee7fd034c6c
doyeoo <ehdusla013@gmail.com>
2023-11-13 15:03:49 +0900

    [feat] 카테고리별로 뱃지 그룹화

--- src/main/java/sungdong29/backend/domain/badge/BadgeController.java
+++ src/main/java/sungdong29/backend/domain/badge/BadgeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sungdong29.backend.domain.badge.dto.response.BadgesResponseDTO;
 import sungdong29.backend.domain.badge.service.BadgeService;
+import sungdong29.backend.global.config.LoginUser;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,8 +18,8 @@ public class BadgeController {
     private final BadgeService badgeService;
 
     @GetMapping("/user/badges")
-    public ResponseEntity<BadgesResponseDTO> findUserBadges() {
-        BadgesResponseDTO badgesResponseDTO = badgeService.findUserBadges(4L);
+    public ResponseEntity<BadgesResponseDTO> findUserBadges(@LoginUser Long userId) {
+        BadgesResponseDTO badgesResponseDTO = badgeService.findUserBadges(userId);
         return ResponseEntity.ok(badgesResponseDTO);
     }
 }
--- src/main/java/sungdong29/backend/domain/badge/dto/response/BadgesResponseDTO.java
+++ src/main/java/sungdong29/backend/domain/badge/dto/response/BadgesResponseDTO.java
@@ -4,17 +4,25 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 public class BadgesResponseDTO {
-    List<BadgeResponseDTO> badges;
+    private List<GroupedBadgeDTO> groupedBadges;
 
-    private BadgesResponseDTO(List<BadgeResponseDTO> badges) {
-        this.badges = badges;
+    public static BadgesResponseDTO from(List<BadgeResponseDTO> badgeResponseDTOS) {
+        BadgesResponseDTO responseDTO = new BadgesResponseDTO();
+        responseDTO.groupedBadges = groupBadgesByCategory(badgeResponseDTOS);
+        return responseDTO;
     }
 
-    public static BadgesResponseDTO from(List<BadgeResponseDTO> badges) {
-        return new BadgesResponseDTO(badges);
+    private static List<GroupedBadgeDTO> groupBadgesByCategory(List<BadgeResponseDTO> badgeResponseDTOS) {
+        return badgeResponseDTOS.stream()
+                .collect(Collectors.groupingBy(BadgeResponseDTO::getCategory))
+                .entrySet()
+                .stream()
+                .map(category -> new GroupedBadgeDTO(category.getKey(), category.getValue()))
+                .collect(Collectors.toList());
     }
 }
