406ebed2f703eeede2f873233719e484ad3d4009
suhhyun524 <2835223@ewhain.net>
2024-02-26 16:04:31 +0900

    [refact] enum 코드 리팩토링 #35

--- src/main/java/sungdong29/backend/domain/event/domain/Category.java
+++ src/main/java/sungdong29/backend/domain/event/domain/Category.java
@@ -1,12 +1,8 @@
 package sungdong29.backend.domain.event.domain;
 
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-
-import java.util.stream.Stream;
 
 @Getter
 @RequiredArgsConstructor
@@ -19,13 +15,5 @@ public enum Category {
     PARK("공원탐방"),
     FOREST("산림여가");
 
-    @JsonValue private final String category;
-
-    @JsonCreator
-    public static Category parsing(String inputValue) {
-        return Stream.of(Category.values())
-                .filter(category -> category.getCategory().equals(inputValue))
-                .findFirst()
-                .orElse(null);
-    }
+    private final String category;
 }
