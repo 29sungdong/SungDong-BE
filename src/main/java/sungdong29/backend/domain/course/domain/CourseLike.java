7b04714683f8c6d99f9cd330b488fec6f2e331a5
suhhyun <97878992+suhhyun524@users.noreply.github.com>
2024-03-18 23:05:42 +0700

    [fix] add annotation

--- src/main/java/sungdong29/backend/domain/course/domain/CourseLike.java
+++ src/main/java/sungdong29/backend/domain/course/domain/CourseLike.java
@@ -4,6 +4,9 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import sungdong29.backend.domain.user.domain.User;
 
+@Entity
+@Getter
+@NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class CourseLike {
 
     @Id
