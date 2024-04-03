961a3e8ec358918f393c5044a86ae958313b22ed
suhhyun524 <2835223@ewhain.net>
2024-04-04 01:06:26 +0900

    [refact] 행사 카테고리 수정

--- src/main/java/sungdong29/backend/domain/event/domain/Category.java
+++ src/main/java/sungdong29/backend/domain/event/domain/Category.java
@@ -7,13 +7,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Category {
-    EDUCATION("교육체험"),
-    FARM("농장체험"),
-    CULTURE("문화행사"),
-    EXHIBITION("전시/관람"),
-    VOLUNTEER("단체봉사"),
-    PARK("공원탐방"),
-    FOREST("산림여가");
+    PERFORMANCE("공연"),
+    EXHIBITION("전시회"),
+    POPUP("팝업스토어"),
+    EVENT("시설행사");
 
     private final String category;
 }
