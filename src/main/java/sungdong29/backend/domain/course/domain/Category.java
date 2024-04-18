bdff0b596db1bf677aeb778ca497e19a8ba4f026
doyeoo <ehdusla013@gmail.com>
2024-04-18 20:53:41 +0900

    [fix] 코스 카테고리 수정

--- src/main/java/sungdong29/backend/domain/course/domain/Category.java
+++ src/main/java/sungdong29/backend/domain/course/domain/Category.java
@@ -7,21 +7,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Category {
 
-    FAMILY("아이와 함께"),
-    PET("반려견과 함께"),
+    WITH_CHILDREN("아이와 함께"),
+    WITH_PET("반려견과 함께"),
     FOR_ME("나만 알고 싶은"),
     FOR_WALK("산책하기 좋은"),
     FOR_DATE("데이트하기 좋은"),
-    ATTRACTIONS("볼거리가 많은"),
+    PICNIC("피크닉"),
     QUIET("조용한"),
     PARKING("주차가능한"),
-    SCENIC("뷰맛집"),
-    FOOD_TOUR("맛집 투어"),
     SEOUL_FOREST("서울숲"),
-    PICNIC("피크닉"),
-    UNIQUE_EXPERIENCE("이색체험"),
-    SUNGDONG_SELECTED("성동산책이 직접 선별한"),
-    SUNGDONG_RECOMMENDED("구민이 추천하는");
+    BY_SERVICE("성동산책이 직접 선별한"),
+    BY_RESIDENTS("구민이 추천하는");
 
     private final String category;
 }
