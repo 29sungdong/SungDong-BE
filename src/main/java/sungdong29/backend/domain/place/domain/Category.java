41bd955aac70fe05dd28d1246237d957008f308e
suhhyun524 <2835223@ewhain.net>
2024-05-22 16:21:39 +0900

    [add] enum 추가

--- src/main/java/sungdong29/backend/domain/place/domain/Category.java
+++ src/main/java/sungdong29/backend/domain/place/domain/Category.java
@@ -9,13 +9,16 @@ import lombok.RequiredArgsConstructor;
 public enum Category {
     CAFE("카페"),
     RESTAURANT("음식점"),
-    FACILITY("시설"),
-    PARK("공원"),
+    FACILITY("체육시설"),
+    NATURE("공원/자연"),
     CULTURE("복합문화공간"),
     KIDS_CAFE("키즈카페"),
     EXPERIENCE("이색체험"),
     EDUCATION("교육관"),
-    WITH_EVENT("행사중");
+    WITH_EVENT("행사중"),
+    LIBRARY("도서관"),
+    MARKET("시장"),
+    STREET("거리");
 
     private final String category;
 }
